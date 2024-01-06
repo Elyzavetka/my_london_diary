4d364d1a2ed43e99af1f932ff6879721afdf776a
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-01-06 23:57:17 +0000

    Change PhotoPost component

--- src/components/PhotoPost.js
+++ src/components/PhotoPost.js
@@ -1,5 +1,18 @@
-const PhotoPost = () => {
-
-}
+const PhotoPost = ({ photo, text, comments }) => {
+    return (
+        <div className="photo-post">
+            <img src={photo} alt="Post" />
+            <p>{text}</p>
+            <div className="comments">
+            <h3>Comments</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                <li key={index}>{comment}</li>
+                ))}
+            </ul>
+            </div>
+        </div>
+    );
+};
 
 export default PhotoPost;
