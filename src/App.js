f24d1476985b0d1cf11831bea90a3282498ba8d0
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-01-21 13:28:23 +0000

    Renamed Hrader component to NavBar and add favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Header from './components/Header';
+import NavBar from './components/NavBar';
 import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <NavBar />
       <Gallery />
     </div>
   );
