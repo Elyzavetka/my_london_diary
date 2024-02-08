9a5066422b4b663c48df674cfb9470bffaf9ae75
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-02-08 18:03:33 +0000

    Fixed the problem with displaying test posts

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,28 +1,30 @@
 import { useState, useEffect } from 'react';
-import PhotoPost from './PhotoPost';
+import PhotoPost from "./PhotoPost";
 
 const Gallery = () => {
+
     const [posts, setPosts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=> {
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((res) => res.json())
+        .then((response) => response.json())
         .then((posts) => {
             setPosts(posts)
         })
         .catch((error) => console.log(error.message))
-        .finally(() => setIsLoading(true))
-    }, [])
+        .finally(() => setIsLoading(false))
+    } ,[])
+
 
     return (
-        <>
+        <div>
             <h1>Posts</h1>
             <hr />
-            {isLoading ? <h1>Loading...</h1> : posts.map((post) => 
+            {isLoading ? <h1>Loading</h1> : posts.map((post) => 
                 <PhotoPost key={post.id} {...post}/>)}
             
-        </>
+        </div>
     )
 }
 
--- src/components/PhotoPost.js
+++ src/components/PhotoPost.js
@@ -1,19 +1,33 @@
-const PhotoPost = ({ photo, text, comments }) => {
-    return 
-        // (
-        // <div className="photo-post">
-        //     <img src={photo} alt="Post" />
-        //     <p>{text}</p>
-        //     <div className="comments">
-        //     <h3>Comments</h3>
-        //     <ul>
-        //         {comments.map((comment, index) => (
-        //         <li key={index}>{comment}</li>
-        //         ))}
-        //     </ul>
-        //     </div>
-        // </div>
-    // )
-};
+// import './Post.css'
+
+const PhotoPost = ({ id, title, userId, body}) => {
+    return (
+        <div className='post'>
+            <small>{id}</small>
+            <h2>{title}</h2>
+            <p>{body}</p>
+            <h3>User ID: {userId}</h3>
+        </div>
+    )
+}
 
 export default PhotoPost;
+
+// const PhotoPost = ({ photo, text, comments }) => {
+//     return (
+//         <div className="photo-post">
+//             <img src={photo} alt="Post" />
+//             <p>{text}</p>
+//             <div className="comments">
+//             <h3>Comments</h3>
+//             <ul>
+//                 {comments.map((comment, index) => (
+//                 <li key={index}>{comment}</li>
+//                 ))}
+//             </ul>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default PhotoPost;
