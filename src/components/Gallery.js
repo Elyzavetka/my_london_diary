a64963795dc13549b5ab1f783e4f233884611a9a
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-02-08 17:43:24 +0000

    added useState and useeffect to the Gallery.js for to get test postd from jsonplaceholder

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,29 @@
-const Gallery = () => {
+import { useState, useEffect } from 'react';
+import PhotoPost from './PhotoPost';
 
+const Gallery = () => {
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(()=> {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => res.json())
+        .then((posts) => {
+            setPosts(posts)
+        })
+        .catch((error) => console.log(error.message))
+        .finally(() => setIsLoading(true))
+    }, [])
+
+    return (
+        <>
+            <h1>Posts</h1>
+            <hr />
+            {isLoading ? <h1>Loading...</h1> : posts.map((post) => 
+                <PhotoPost key={post.id} {...post}/>)}
+            
+        </>
+    )
 }
 
 export default Gallery;
