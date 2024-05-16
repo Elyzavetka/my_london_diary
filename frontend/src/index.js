c223ebe9de36fc0c68f3f6f435295b132e9a25a8
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-05-17 00:58:20 +0100

    added custom cursor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,26 @@ root.render(
     <App />
   </React.StrictMode>
 );
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  cursorDot.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, { duration: 800, fill: "forwards" }); 
+
+  cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+  }, { duration: 3000, fill: "forwards" });
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
