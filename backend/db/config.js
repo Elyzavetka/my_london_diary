d6c3a0cc95f04a59d8a7761eec1740d986373d02
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-10-20 22:15:16 +0100

    Move username and password to dotenv

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // user: "liza",
-  // password: "12345",
-  host: "localhost",
-  database: "my_london_diary",
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = {
