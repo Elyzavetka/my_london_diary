b970043cb430d3f4ae6130dc477cdd09dcd1cbd1
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-10-28 17:09:34 +0000

    Delete unused imports.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Gallery from "./components/Gallery/Gallery";
-
-import Feed from "./components/Feed";
-import Recomendations from "./components/Recommendations/Recommendations";
-import Geolocation from "./components/Geolocation";
 import { Home } from "./pages/Home";
 import { NewPost } from "./pages/NewPost/NewPost";
 import LoginPage from "./pages/Auth/LoginPage";
