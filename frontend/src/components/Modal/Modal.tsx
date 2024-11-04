9dc8e9b0a305ceefd06b8a4fa60382946f2fe0d4
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-11-04 18:34:58 +0000

    Add styles module for Modal.tsx

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -1,18 +1,30 @@
 import React from "react";
+import styles from "./Modal.module.css";
+
 const Modal = ({ isOpen, onClose, address }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="tips-modal">
-      <div className="tips-modal-form">
-        <button onClick={onClose}>X</button>
-        <form>
+    <div className={styles.modalOverlay}>
+      <div className={`${styles.formContainer} ${styles.tipsModalForm}`}>
+        <button className={styles.closeButton} onClick={onClose}>
+          X
+        </button>
+        <form className={styles.formContent}>
           <p>📍{address}</p>
-          <textarea />
-          <button type="submit">submit</button>
-          <div>
-            <input type="file" id="img" name="img" accept="image/*" />
+          <textarea className={styles.formTextArea} />
+          <div className={styles.formField}>
+            <input
+              type="file"
+              id="img"
+              name="img"
+              accept="image/*"
+              className={styles.fileUpload}
+            />
           </div>
+          <button type="submit" className={styles.formButton}>
+            Submit
+          </button>
         </form>
       </div>
     </div>
