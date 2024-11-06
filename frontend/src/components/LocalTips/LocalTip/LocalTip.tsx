634754a3565d8a026a090b69194dc1d540f8c943
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-11-06 23:56:48 +0000

    Add styles for LocalTip component.

--- frontend/src/components/LocalTips/LocalTip/LocalTip.tsx
+++ frontend/src/components/LocalTips/LocalTip/LocalTip.tsx
@@ -1,7 +1,15 @@
+import React from "react";
 import styles from "./LocalTip.module.css";
 
 const LocalTip = ({ el }) => {
-  return <div>{el.description}</div>;
+  return (
+    <>
+      <button className={styles.tipItem}>
+        <span className={styles.geolocation}>📌 {el.geolocation}</span>
+        <span className={styles.description}>{el.description}</span>
+      </button>
+    </>
+  );
 };
 
 export default LocalTip;
