d53aa46e82ef096e2bc1440382ac3879941f409f
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-11-07 23:23:55 +0000

    Add styles.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Feed from "../components/Feed";
 import LocalTips from "../components/LocalTips/LocalTips";
 import { useAuth } from "../context/AuthContext";
+import styles from "./Home.module.css";
 
 export const Home = () => {
   const { username } = useAuth();
   return (
     <>
       <h1>My London Diary</h1>
-      {username && <h2>Hello, {username}</h2>}
+      {username && <div className={styles.userName}>Hello, {username}</div>}
       <div className="appContainer">
         <div className="feed">
           <Feed />
